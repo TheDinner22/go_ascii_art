0865a93b87afdd61d5c6ac4c600e33b53bf60e18
Joseph Handsome <nottellingu@gmail.com>
2024-07-01 11:55:37 -0400

    how about now

--- example/example.go
+++ example/example.go
@@ -2,7 +2,7 @@ package main
 
 import (
     imageReader "github.com/TheDinner22/go_ascii_art/image_reader"
-    imageDisplayer "github.com/TheDinner22/go_ascii_art/terminal_image_displayer"
+    imageDisplayer "github.com/TheDinner22/go_ascii_art/image_displayer"
     imageProcessor "github.com/TheDinner22/go_ascii_art/image_processor"
 )
 
