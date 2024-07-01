192e07ee1757b1f46038c47204d15c5620764354
Joseph Handsome <nottellingu@gmail.com>
2024-07-01 12:30:35 -0400

    runes working and brigtness matrix
    i haven't even run the code yet so im sure it just does not work
    also thinking there is room here for some concurency optimizations

--- image_processor/image_processor.go
+++ image_processor/image_processor.go
@@ -1,9 +1,34 @@
 package imageprocessor
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"image"
+)
 
-var chars = ["`", "\^", "\"", ",", ":", ";", "I", "l", "!", "i", "~", "+", "_", "-", "?", "]", "[", "}", "{", "1", ")", "(", "|", "\\", "/", "t", "f", "j", "r", "x", "n", "u", "v", "c", "z", "X", "Y", "U", "J", "C", "L", "Q", "0", "O", "Z", "m", "w", "q", "p", "d", "b", "k", "h", "a", "o", "*", "#", "M", "W", "&", "8", "%", "B", "@", "$"]
+var chars = [...]rune{'`', '^', '\'', ',', ':', ';', 'I', 'l', '!', 'i', '~', '+', '_', '-', '?', ']', '[', '}', '{', '1', ')', '(', '|', '\\', '/', 't', 'f', 'j', 'r', 'x', 'n', 'u', 'v', 'c', 'z', 'X', 'Y', 'U', 'J', 'C', 'L', 'Q', '0', 'O', 'Z', 'm', 'w', 'q', 'p', 'd', 'b', 'k', 'h', 'a', 'o', '*', '#', 'M', 'W', '&', '8', '%', 'B', '@', '$'}
 
-func Test(){
-    fmt.Println("processor")
+func GetBrightnessMatrix(img image.Image) ([][]int, error) {
+	if img == nil {
+		return nil, errors.New("image was nil?!")
+	}
+
+	width := img.Bounds().Dx()
+	height := img.Bounds().Dy()
+	brightnessMatrix := make([][]int, height)
+
+	for i := 0; i < height; i++ {
+		row := make([]int, width)
+		for j := 0; j < width; j++ {
+            r, g, b, _ := img.At(i, j).RGBA()
+            row[j] = (int(r)+int(g)+int(b))/3
+		}
+		brightnessMatrix[i] = row
+	}
+
+	return brightnessMatrix, nil
+}
+
+func Test() {
+	fmt.Println("processor")
 }
