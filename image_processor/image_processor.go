e20393af432a445690b8d1a72ab8f3e286dc3aa0
Joseph Handsome <nottellingu@gmail.com>
2024-07-02 17:51:52 -0400

    now getting a slice of runes from an image

--- image_processor/image_processor.go
+++ image_processor/image_processor.go
@@ -2,7 +2,6 @@ package imageprocessor
 
 import (
 	"errors"
-	"fmt"
 	"image"
 )
 
@@ -20,10 +19,10 @@ func GetBrightnessMatrix(img image.Image) ([][]float64, error) {
 	for i := 0; i < height; i++ {
 		row := make([]float64, width)
 		for j := 0; j < width; j++ {
-            r, g, b, _ := img.At(i, j).RGBA()
-            average_brightness := (float64(r)+float64(g)+float64(b))/ float64(3)
-            normalized_average_brightness := average_brightness / float64(255)
-            row[j] = normalized_average_brightness // ranges from 0-1 inclusive
+			r, g, b, _ := img.At(i, j).RGBA()
+			average_brightness := (float64(r) + float64(g) + float64(b)) / float64(3)
+			normalized_average_brightness := average_brightness / float64(255)
+			row[j] = normalized_average_brightness // ranges from 0-1 inclusive
 		}
 		brightnessMatrix[i] = row
 	}
@@ -31,6 +30,20 @@ func GetBrightnessMatrix(img image.Image) ([][]float64, error) {
 	return brightnessMatrix, nil
 }
 
-func Test() {
-	fmt.Println("processor")
+func MapBrightnessMatrixToChars(bm [][]float64) [][]rune {
+	return mmap(bm, func(row []float64) []rune {
+		return mmap(row, func(pixel float64) rune {
+			index := int(pixel * 64) // there are 65 chars total
+			return chars[index]
+		})
+	})
+}
+
+// https://stackoverflow.com/questions/71624828/is-there-a-way-to-map-an-array-of-objects-in-go
+func mmap[T, U any](ts []T, f func(T) U) []U {
+	us := make([]U, len(ts))
+	for i := range ts {
+		us[i] = f(ts[i])
+	}
+	return us
 }
