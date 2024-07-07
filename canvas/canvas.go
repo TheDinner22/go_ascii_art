be86685889d01e799f1c79328bfa9b3c20e6e674
Joseph Handsome <nottellingu@gmail.com>
2024-07-06 22:36:49 -0400

    ughhhhhhhhh

--- canvas/canvas.go
+++ canvas/canvas.go
@@ -5,8 +5,10 @@ import (
 	"io"
 	"os"
 
+	imageProcessor "github.com/TheDinner22/go_ascii_art/image_processor"
+
+	"github.com/nfnt/resize"
 	"golang.org/x/term"
-    "github.com/nfnt/resize"
 )
 
 // so you can make your own versions of this for different outputs but one for the terminal is made for you
@@ -23,36 +25,15 @@ func FitImageToCanvas(img image.Image, canvas Canvas) [][]float64 {
 	imageWidthSmallerThanCanvasWidth := imgWidth < canvasWidth
 	imageHeightSmallerThanCanvasHeight := imgHeight < canvasHeight
 
-    // if the image is too big, resize it
+	// if the image is too big, resize it
 	if !(imageHeightSmallerThanCanvasHeight && imageWidthSmallerThanCanvasWidth) {
-        img = resize.Resize(uint(canvasWidth), 0, img, resize.NearestNeighbor)
+		img = resize.Resize(uint(canvasWidth), 0, img, resize.NearestNeighbor)
 	}
 
-    // convert image to a slice (each element is a char to be printed)
-    // (right now tho, they are values from 0-1 representing lightness or darkness)
-    // 0 is light and 1 is dark
-    return imageToSlice(img)
-}
-
-func imageToSlice(img image.Image) [][]float64 {
-	// max value that r g and b can be from the pixel color values
-	const MAX_COLOR_VALUE float64 = 0xffff
-
-	imgWidth := img.Bounds().Dx()
-	imgHeight := img.Bounds().Dy()
-
-	result := make([][]float64, imgHeight)
-	for i := 0; i < imgHeight; i++ {
-		row := make([]float64, imgWidth)
-		for j := 0; j < imgWidth; j++ {
-			r, g, b, _ := img.At(j, i).RGBA()
-			average_color := (float64(r) + float64(b) + float64(g)) / float64(3)
-			normalized_average_color := average_color / MAX_COLOR_VALUE
-			row[j] = normalized_average_color
-		}
-		result[i] = row
-	}
-	return result
+	// convert image to a slice (each element is a char to be printed)
+	// (right now tho, they are values from 0-1 representing lightness or darkness)
+	// 0 is light and 1 is dark
+	return imageProcessor.ImageToSlice(img)
 }
 
 // todo crash if not a terminal as stdout
--- image_processor/image_processor.go
+++ image_processor/image_processor.go
@@ -1,5 +1,7 @@
 package imageprocessor
 
+import "image"
+
 var chars = [...]rune{'`', '^', '\'', ',', ':', ';', 'I', 'l', '!', 'i', '~', '+', '_', '-', '?', ']', '[', '}', '{', '1', ')', '(', '|', '\\', '/', 't', 'f', 'j', 'r', 'x', 'n', 'u', 'v', 'c', 'z', 'X', 'Y', 'U', 'J', 'C', 'L', 'Q', '0', 'O', 'Z', 'm', 'w', 'q', 'p', 'd', 'b', 'k', 'h', 'a', 'o', '*', '#', 'M', 'W', '&', '8', '%', 'B', '@', '$'}
 
 func MapBrightnessMatrixToChars(bm [][]float64) [][]rune {
@@ -10,6 +12,28 @@ func MapBrightnessMatrixToChars(bm [][]float64) [][]rune {
 		})
 	})
 }
+
+func ImageToSlice(img image.Image) [][]float64 {
+	// max value that r g and b can be from the pixel color values
+	const MAX_COLOR_VALUE float64 = 0xffff
+
+	imgWidth := img.Bounds().Dx()
+	imgHeight := img.Bounds().Dy()
+
+	result := make([][]float64, imgHeight)
+	for i := 0; i < imgHeight; i++ {
+		row := make([]float64, imgWidth)
+		for j := 0; j < imgWidth; j++ {
+			r, g, b, _ := img.At(j, i).RGBA()
+			average_color := (float64(r) + float64(b) + float64(g)) / float64(3)
+			normalized_average_color := average_color / MAX_COLOR_VALUE
+			row[j] = normalized_average_color
+		}
+		result[i] = row
+	}
+	return result
+}
+
 
 // https://stackoverflow.com/questions/71624828/is-there-a-way-to-map-an-array-of-objects-in-go
 func mmap[T, U any](ts []T, f func(T) U) []U {
